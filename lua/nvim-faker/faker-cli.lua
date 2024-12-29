b1802d052c188a289792aa8834543ba09c3bf1c6
tehdb <tehdb@me.com>
2024-12-29 15:03:35 +0100

    adds autocompletion for modules and functions

--- lua/nvim-faker/faker-cli.lua
+++ lua/nvim-faker/faker-cli.lua
@@ -1,29 +1,59 @@
-local M = {}
-
+local M = {
+  faker_modules_map = {},
+}
 local semver_pattern = '^%d+%.%d+%.%d+$'
+local config = {}
 
-function M.is_globally_installed(npm_package)
-  local output = vim.fn.system('faker --info')
-  return output:sub(1, 17) == npm_package
-end
-
-function M.ensure_npx_cache(npm_package)
-  local cmd = 'npx --yes ' .. npm_package .. ' --version'
-  local result = vim.fn.system(cmd)
-  if result:match(semver_pattern) then
-    return true
-  else
-    return false
-  end
-end
-
-function M.execute_faker_cli_command(params, use_global_package, npm_package)
+local function getCmd()
   local fakerCmd = ''
-  if use_global_package then
+  if config.use_global_package then
     fakerCmd = 'faker'
   else
-    fakerCmd = 'npx --yes ' .. npm_package
+    fakerCmd = 'npx --yes ' .. config.npm_package
   end
+
+  return fakerCmd
+end
+
+local function trim_and_remove_newlines(str)
+  -- Remove newline characters
+  str = str:gsub('\n', '')
+  -- Trim leading and trailing whitespace
+  str = str:match('^%s*(.-)%s*$')
+  return str
+end
+
+local function cache_availeble_modules()
+  -- if not empty then return the in memory cached list
+  -- if next(faker_modules_map) ~= nil then
+  --   return faker_modules_map
+  -- end
+
+  local modules_map = {}
+  local fakerCmd = getCmd()
+
+  local output = vim.fn.system(fakerCmd .. '  --available-modules')
+
+  local current_key = nil
+
+  for line in output:gmatch('[^\r\n]+') do
+    local key = line:match('^(%w+):$')
+    if key then
+      current_key = key
+      modules_map[current_key] = {}
+    elseif current_key then
+      local value = line:match('^%s*-%s*(%w+)$')
+      if value then
+        table.insert(modules_map[current_key], value)
+      end
+    end
+  end
+
+  M.faker_modules_map = modules_map
+end
+
+function M.execute_faker_cli_command(params)
+  local fakerCmd = getCmd()
 
   local output = vim.fn.system(fakerCmd .. ' ' .. params)
   local status = vim.v.shell_error
@@ -45,4 +75,27 @@ function M.execute_faker_cli_command(params, use_global_package, npm_package)
   end
 end
 
+function M.is_globally_installed(npm_package)
+  local output = vim.fn.system('faker --info')
+  output = trim_and_remove_newlines(output)
+  return output:sub(1, string.len(npm_package)) == npm_package
+end
+
+function M.ensure_npx_cache(npm_package)
+  local output = vim.fn.system('npx --yes ' .. npm_package .. ' --version')
+  output = trim_and_remove_newlines(output)
+
+  if output:match(semver_pattern) then
+    return true
+  else
+    return false
+  end
+end
+
+function M.init(use_global_package, npm_package)
+  config.use_global_package = use_global_package
+  config.npm_package = npm_package
+
+  cache_availeble_modules()
+end
 return M
--- lua/nvim-faker/init.lua
+++ lua/nvim-faker/init.lua
@@ -2,24 +2,49 @@ local api = vim.api
 local faker_cli = require('nvim-faker.faker-cli')
 
 local M = {}
+local fakerCliAvaileble = false
 
 local config = {
   -- TODO:
   -- locale = 'en',
+  -- npm_package_verison = "0.5.1"
   npm_package = '@tehdb/faker-cli',
   use_global_package = false,
 }
 
 function M.setup(user_config)
   config = vim.tbl_deep_extend('force', config, user_config or {})
+
+  vim.schedule(function()
+    if config.use_global_package == true then
+      fakerCliAvaileble = faker_cli.is_globally_installed(config.npm_package)
+    else
+      fakerCliAvaileble = faker_cli.ensure_npx_cache(config.npm_package)
+    end
+
+    if fakerCliAvaileble then
+      faker_cli.init(config.use_global_package, config.npm_package)
+    end
+  end)
 end
 
 local function print_error(error, show_help)
-  show_help = show_help or true
+  if show_help == nil then
+    show_help = 'usage'
+  end
+
   vim.api.nvim_err_writeln(error)
 
-  if show_help then
+  if show_help == 'usage' then
     print('Usage: Faker <module> <function> [param-value]... [param-key:param-value]... [locale]')
+  elseif show_help == 'install' then
+    if config.use_global_package == false then
+      print('Check if the package ' .. config.npm_package .. ' exists in the npm registry')
+    else
+      print(
+        "Run 'npm install --global " .. config.npm_package .. "' to install the package globally"
+      )
+    end
   end
 end
 
@@ -46,9 +71,47 @@ local function insert_text(text)
   api.nvim_buf_set_text(buf, start_row, start_col, end_row, end_col, lines)
 end
 
+local function command_args_completion(arg_lead, cmd_line, cursor_pos)
+  local faker_modules_map = faker_cli.faker_modules_map
+
+  local module_keys = {}
+  for key, _ in pairs(faker_modules_map) do
+    table.insert(module_keys, key)
+  end
+
+  table.sort(module_keys)
+
+  local args = vim.split(cmd_line, '%s+')
+
+  if #args == 2 then
+    return vim.tbl_filter(function(val)
+      return vim.startswith(val, arg_lead)
+    end, module_keys)
+  elseif #args == 3 then
+    local first_arg = args[2]
+    local module_function_keys = faker_modules_map[first_arg] or {}
+    table.sort(module_function_keys)
+
+    return vim.tbl_filter(function(val)
+      return vim.startswith(val, arg_lead)
+    end, module_function_keys)
+  else
+    return {}
+  end
+end
+
 api.nvim_create_user_command('Faker', function(opts)
-  local output, err =
-    faker_cli.execute_faker_cli_command(opts.args, config.use_global_package, config.npm_package)
+  if fakerCliAvaileble == false then
+    print_error('Error: Faker CLI is not available', 'install')
+    return
+  end
+
+  if not vim.bo.modifiable then
+    print_error("':Faker' command available only in modifiable buffers", false)
+    return
+  end
+
+  local output, err = faker_cli.execute_faker_cli_command(opts.args)
 
   if output == nil then
     if err then
@@ -63,6 +126,7 @@ api.nvim_create_user_command('Faker', function(opts)
   insert_text(output)
 end, {
   nargs = '+',
+  complete = command_args_completion,
 })
 
 return M
